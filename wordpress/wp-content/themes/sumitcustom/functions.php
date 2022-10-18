da15f4ec1cd4f3f2698b880ba846dcc7b1c7c5a1
sumit <webgrity49@gmail.com>
2022-10-18 18:45:16 +0530

    add a new template

--- wordpress/wp-content/themes/sumitcustom/functions.php
+++ wordpress/wp-content/themes/sumitcustom/functions.php
@@ -45,3 +45,25 @@ function my_login_logo(){
     <?php
 }
 add_action('login_enqueue_scripts', 'my_login_logo');
+
+function my_plugin_menu(){
+    add_options_page( 'My Plugin Options', 'My Plugin', 'manage_options', 'my-unique-identifier', 'my_plugin_options');
+}
+add_action('admin_menu', 'my_plugin_menu');
+
+function my_plugin_options(){
+    if(!current_user_can('manage_options')){
+        wp_die( __('You do not have sufficent permission to access this page'));
+    }
+    echo '<div class="wrap">';
+    echo '<p> Here is where the form would go if I actually had options</p>';
+    echo '</div>';
+}
+
+    // add_menu_page(
+    //     'Sumit Custom',
+    //     'Sumit menu',
+    //     'manage_options',
+    //     'sumit-custom',
+    //     'display_the_contents'
+    // );
---
+++ wordpress/wp-content/themes/sumitcustom/sumit.php
@@ -0,0 +1,2 @@
+<!-- Template Name: Sumit -->
+Sumit
